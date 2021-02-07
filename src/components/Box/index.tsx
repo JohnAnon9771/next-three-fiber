2b568f65325789c5bfd499a47712398482d8e30f
JohnAnon9771 <njoao97710@gmail.com>
2021-02-07 03:01:03 +0000

    :sparkles: FEAT: add animation in Box component

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,6 +1,8 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useFrame, MeshProps, BoxGeometryProps } from 'react-three-fiber'
 
+import { useSpring, a } from '@react-spring/three'
+import { MeshWobbleMaterial } from '@react-three/drei'
 import { Mesh } from 'three'
 
 export default function Box({
@@ -9,15 +11,26 @@ export default function Box({
   color,
 }: (MeshProps & BoxGeometryProps) & { color: string }): JSX.Element {
   const refBox = useRef<Mesh>(null)
+  const [expand, setExpand] = useState(false)
 
   useFrame(() => {
     refBox.current.rotation.x = refBox.current.rotation.y += 0.01
   })
 
+  const { scale } = useSpring({
+    scale: expand ? [1.5, 1.5, 1.5] : [1, 1, 1],
+  })
+
   return (
-    <mesh castShadow position={position} ref={refBox} scale={[1, 1, 1]}>
+    <a.mesh
+      onClick={() => setExpand(state => !state)}
+      castShadow
+      position={position}
+      ref={refBox}
+      scale={scale}
+    >
       <boxGeometry attach="geometry" args={args} />
-      <meshPhongMaterial color={color} />
-    </mesh>
+      <MeshWobbleMaterial color={color} />
+    </a.mesh>
   )
 }
