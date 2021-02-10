6177908b9f4198b7d95bf16be2e4bb64626e05a2
JohnAnon9771 <njoao97710@gmail.com>
2021-02-10 20:51:08 +0000

    :sparkles: FEAT: add links in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import * as S from './styles'
 
 export default function Header(): JSX.Element {
@@ -6,6 +8,14 @@ export default function Header(): JSX.Element {
       <div className="start">
         <h3>React Three Fiber</h3>
         <p>JohnAnon9771</p>
+        <S.Links>
+          <Link href="/first-example">
+            <a>First Example</a>
+          </Link>
+          <Link href="/second-example">
+            <a>Second Example</a>
+          </Link>
+        </S.Links>
       </div>
       <strong>0.1.0</strong>
     </S.Header>
--- src/pages/second-example.tsx
+++ src/pages/second-example.tsx
@@ -4,20 +4,22 @@ import {Canvas} from 'react-three-fiber'
 
 import {OrbitControls} from '@react-three/drei'
 
+import {Plane} from 'components'
+
 const Earth = dynamic(
   () => import('components').then(component => component.Earth),
-  {ssr: false}
+  {
+    ssr: false,
+  }
 )
 
 export default function SecondExample(): JSX.Element {
   return (
-    <Canvas
-      style={{width: '100%', height: '100%'}}
-      camera={{position: [0, 0, 10], fov: 100}}
-    >
+    <Canvas camera={{position: [0, 15, 0], fov: 75}}>
+      <Earth />
+      <Plane />
       <ambientLight intensity={0.3} />
       <spotLight intensity={0.5} position={[0, 50, 0]} />
-      <Earth />
       <OrbitControls />
     </Canvas>
   )
