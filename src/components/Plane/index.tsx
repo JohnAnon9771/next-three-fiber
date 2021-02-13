87b18be28df0f5e21881f1bfd98764c43a33d21e
JohnAnon9771 <njoao97710@gmail.com>
2021-02-13 21:36:47 +0000

    :sparkles: FEAT: add color

--- src/components/Plane/index.tsx
+++ src/components/Plane/index.tsx
@@ -1,8 +1,12 @@
-export default function Plane(): JSX.Element {
+interface Props {
+  color?: string
+}
+
+export default function Plane({color = '#eee'}: Props): JSX.Element {
   return (
     <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
       <planeGeometry args={[100, 100]} />
-      <meshStandardMaterial />
+      <meshStandardMaterial color={color} />
     </mesh>
   )
 }
