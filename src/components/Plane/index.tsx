c087b42ba7f3ff79769de622c00e56b0d70936b2
João Alves <njoao97710@gmail.com>
2021-02-28 23:29:34 -0300

    :hammer: FIX: refact plane component and thirty

--- src/components/Plane/index.tsx
+++ src/components/Plane/index.tsx
@@ -1,11 +1,15 @@
 interface Props {
   color?: string
+  args?: [number?, number?]
 }
 
-export default function Plane({color = '#eee'}: Props): JSX.Element {
+export default function Plane({
+  color = '#eee',
+  args = [100, 100],
+}: Props): JSX.Element {
   return (
     <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
-      <planeGeometry args={[100, 100]} />
+      <planeGeometry args={args} />
       <meshStandardMaterial color={color} />
     </mesh>
   )
