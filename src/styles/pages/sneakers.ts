d4587aeead0af0391a39268dd747ce45235500f8
JohnAnon9771 <njoao97710@gmail.com>
2021-03-18 19:29:24 +0000

    :sparkles: FEAT: add styles main

--- src/styles/pages/sneakers.ts
+++ src/styles/pages/sneakers.ts
@@ -1,15 +1,21 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  /* background-color: var(--bg-primary); */
+  background-color: var(--bg-primary);
 
   height: 100vh;
   width: 100vw;
 
   .header {
     position: absolute;
+    display: flex;
+    align-items: center;
+
+    z-index: 1;
 
     background-color: var(--white);
+    height: 100px;
+    width: 100%;
 
     &__nav {
       display: flex;
@@ -17,5 +23,14 @@ export const Container = styled.div`
       justify-content: space-around;
       align-items: center;
     }
+  }
+`
+
+export const Main = styled.main`
+  height: 100vh;
+  width: 100vw;
+
+  .canvas {
+    z-index: 0;
   }
 `
